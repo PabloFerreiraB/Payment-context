8dd17f5169cbf701bfca274c0f49281f3258dae7
Pablo Ferreira <Pablo_Ferreira14@hotmail.com>
2019-07-31 13:03:05 -0300

    Criado as entidades.

---
+++ PaymentContext.Domain/Entities/Payment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PaymentContext.Domain.Entities
+{
+    public abstract class Payment
+    {
+        public string Number { get; private set; }
+        public DateTime PaidDate { get; private set; }
+        public DateTime ExpireDate { get; private set; }
+        public decimal Total { get; private set; }
+        public decimal TotalPaid { get; private set; }
+        public string Payer { get; private set; }
+        public string Document { get; private set; }
+        public string Address { get; private set; }
+        public string Email { get; private set; }
+    }
+
+    public class BoletoPayment : Payment
+    {
+        public string BarCode { get; private set; } // Código de barras
+        public string BoletoNumber { get; private set; } // Número do boleto
+    }
+
+    public class CreditCardPayment : Payment
+    {
+        public string CardHolderName { get; private set; } // Nome do titular
+        public string CardNumber { get; private set; } // Apenas os 4 últimos número do cartão
+        public string LastTransactionNumber { get; private set; } // última transação realizada
+    }
+
+    public class PayPalPayment : Payment
+    {
+        public string TransactCode { get; private set; } // Código da transação
+    }
+}
---
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class Subscription
+    {
+        public DateTime CreateDate { get; private set; }
+        public DateTime LastUpdateDate { get; private set; }
+        public DateTime? ExpireDate { get; private set; }
+        public bool Active { get; private set; }
+        public List<Payment> Payments { get; private set; }
+    }
+}
