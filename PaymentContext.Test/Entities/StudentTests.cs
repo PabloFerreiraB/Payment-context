82abdccd34a7a13fee5334abd02ef4bc89df04ff
Pablo Ferreira <Pablo_Ferreira14@hotmail.com>
2019-08-01 14:47:02 -0300

    Realizado testes apenas nos documentos CNPJ e CPF

--- PaymentContext.Test/Entities/StudentTests.cs
+++ PaymentContext.Test/Entities/StudentTests.cs
@@ -1,24 +1,14 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PaymentContext.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace PaymentContext.Test.Entities
 {
     [TestClass]
     public class StudentTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        //[TestMethod]
+        public void AddSubscription()
         {
-            //var subscription = new Subscription(null);
-            //var student = new Student("Pablo", "Ferreira", "123456789", "pablo@gmail.com");
-
-            //student.AddSubscription(subscription);
-
-
-
+            Assert.Fail();
         }
     }
 }
---
+++ PaymentContext.Test/ValueObjects/DocumentTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Test.ValueObjects
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        // Red, Green, Refactor
+
+        // Passo 1 - Fazer com que todos testes falhar | Assert.Fail();
+        // Passo 2 - Fazer com que todos testes retornar sucesso
+        // Passo 3 - Refator o código de teste
+
+        [TestMethod]
+        public void ShoulReturnErrorWhenCNPJIsInvalid() // Deve retornar erro quando o CNPJ for inválido
+        {
+            var doc = new Document("123", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+        [TestMethod]
+        //[DataTestMethod]
+        //[DataRow("76524455000178")]
+        //[DataRow("43442116000193")]
+        //[DataRow("85403512000151")]
+        public void ShoulReturnErrorWhenCNPJIsValid() // Deve retornar sucesso quando o CNPJ for válido
+        {
+            var doc = new Document("85403512000151", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Valid);
+        }
+
+        [TestMethod]
+        public void ShoulReturnErrorWhenCPFIsInvalid() // Deve retornar erro quando o CPF for inválido
+        {
+            var doc = new Document("123", EDocumentType.CPF);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+        [TestMethod]
+        public void ShoulReturnErrorWhenCPFIsValid() // Deve retornar sucesso quando o CPF for válido
+        {
+            var doc = new Document("38804005807", EDocumentType.CPF);
+            Assert.IsTrue(doc.Valid);
+        }
+    }
+}
